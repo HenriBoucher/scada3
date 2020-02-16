bf19ed56b8097f3a7a3f21d5326daf4a811eeddd
tfdllc136 <tfdllc136@TFDL-PC10103W.uc.ucalgary.ca>
2020-02-16 15:11:30 -0700

    Added scada* and station projects
    Problems  15 items  13 due to eventBroker not being used

---
+++ com.se.pipeline.scadahistory/src/com/se/pipeline/scadahistory/Activator.java
@@ -0,0 +1,27 @@
+package com.se.pipeline.scadahistory;
+
+import javax.inject.Inject;
+
+import org.eclipse.e4.core.services.events.IEventBroker;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+	@Inject
+	private static IEventBroker eventBroker;
+
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+	}
+
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
---
+++ com.se.pipeline.station.station9/src/com/se/pipeline/station/station9/Activator.java
@@ -0,0 +1,27 @@
+package com.se.pipeline.station.station9;
+
+import javax.inject.Inject;
+
+import org.eclipse.e4.core.services.events.IEventBroker;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+	@Inject
+	private static IEventBroker eventBroker;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+	}
+
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
