534a271a40f197f69675674b07c6067ed0519fc4
lib105 <lib105@SAIT223560.ACDM.DS.SAIT.CA>
2020-02-24 16:58:01 -0700

    Modify and use ScadaSystem instead of Activator
    Lot's of changes to ScadaSystem.java.  The neat thing is we can use
    MF's Bundle-Activator header to start stations.
    TODO - ScadaAlarm, ScadaEvent and ScadaHistory
    TODO - setup ScadaStation as a super class

--- com.se.pipeline.scadasystem/src/com/se/pipeline/scadasystem/ScadaSystem.java
+++ com.se.pipeline.scadasystem/src/com/se/pipeline/scadasystem/ScadaSystem.java
@@ -5,12 +5,20 @@ import java.util.Dictionary;
 import javax.inject.Inject;
 
 import org.eclipse.e4.core.di.annotations.Creatable;
+import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
 import org.osgi.service.component.ComponentContext;
 
 @Creatable
-public class ScadaSystem {
+public class ScadaSystem implements BundleActivator {
+	
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
 	
 	@Inject
 	private com.se.pipeline.scadaalarm.ScadaAlarm scadaAlarm;
@@ -19,26 +27,23 @@ public class ScadaSystem {
 	@Inject
 	private com.se.pipeline.scadahistory.ScadaHistory scadaHistory;
 
-// tried start but it's not executed
-//    protected void start(BundleContext bundleContext) throws Exception {
-//    	Dictionary<String, String> headers = bundleContext.getBundle().getHeaders();
-//    	String requireBundle = headers.get(Constants.REQUIRE_BUNDLE);
-//    	int i = 23/0;  // test if code is executed
-//    	String[] bundles = requireBundle.split("\\s*,\\s*");
-//    	for (String bundle : bundles) {
-//    		if (bundle.contains(".station.")) {
-//    			
-//    		}
-//    	}
-//    }
-	
-	protected void activate(ComponentContext componentContext) {
-		Dictionary<String, String> headers = componentContext.getBundleContext().getBundle().getHeaders();  // then as above
-		int i = 2/0;
-	}
-
-    protected void deactivate(ComponentContext componentContext) {}
+    public void start(BundleContext bundleContext) throws Exception {
+    	ScadaSystem.context = bundleContext;
+    	Dictionary<String, String> headers = bundleContext.getBundle().getHeaders();
+    	String requireBundle = headers.get(Constants.REQUIRE_BUNDLE);
+    	String[] bundles = requireBundle.split("\\s*,\\s*");
+    	for (String bundle : bundles) {
+    		if (bundle.contains(".station.")) {
+    			System.out.println(bundle);
+    		}
+    	}
+    }
     
+	public void stop(BundleContext bundleContext) throws Exception {
+		ScadaSystem.context = null;
+		System.err.println("in scadasystem.stop");
+	}
+     
     public com.se.pipeline.scadaalarm.ScadaAlarm getScadaAlarm() { 
     	return scadaAlarm;
     }
